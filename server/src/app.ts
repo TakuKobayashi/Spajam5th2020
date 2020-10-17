9cb322349d5f72dc9f76832466a29f012cc5f97f
Taku Kobayashi <keep_slimbody@yahoo.co.jp>
2020-10-18 06:54:07 +0900

    downloadしたものをS3にアップロードする

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,9 +3,14 @@ import 'source-map-support/register';
 import { APIGatewayEvent, APIGatewayProxyHandler, Context } from 'aws-lambda';
 import * as awsServerlessExpress from 'aws-serverless-express';
 import * as express from 'express';
+import axios from 'axios';
 
+const { v4: uuid } = require('uuid');
 const ffmpeg = require('fluent-ffmpeg');
 const command = ffmpeg();
+
+const aws = require('aws-sdk');
+const s3 = new aws.S3({ apiVersion: '2006-03-01' });
 
 const app = express();
 const server = awsServerlessExpress.createServer(app);
@@ -20,6 +25,9 @@ app.use(bodyParser.json());
 app.use(cors({ origin: true }));
 
 app.get('/', (req: express.Request, res: express.Response) => {
+  const response = await axios.get("https://mov.20th-meijiyasuda-kazumasaoda.jp/upload/jKhEd__2FlIZtUrQVTzXrTfxg__3D__3D/out.mp4", {responseType: 'arraybuffer'})
+  const putResponse = await s3.putObject({Bucket: "taptappun", Key: "project/spajam5th2020/" + uuid() + ".mp4", Body: response.data, ACL: 'public-read'}).promise()
+  console.log(putResponse);
   res.json({ hello: 'world' });
 });
 
